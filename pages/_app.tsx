becf7e37e0e19c8ec33eda0baffce84a453259e9
Alexey <alexey.okorokov.work@gmail.com>
2023-11-24 14:14:44 +0200

    added: react redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,16 @@ import type { AppProps } from 'next/app'
 import React from 'react'
 import '../src/styles/globals.css'
 import '../src/styles/style.scss'
+import {store} from "@redux/store/store";
+import {Provider} from "react-redux";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
+    <Provider store={store}>
       <React.Fragment>
         <Component {...pageProps} />
       </React.Fragment>
+    </Provider>
   )
 }
 
